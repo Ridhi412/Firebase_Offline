87fb10f240a7f4e885eef14fc33c0371c8caefaf
Ridhi M Jain <38811855+Ridhi412@users.noreply.github.com>
2022-09-12 22:54:55 +0530

    Add files via upload

---
+++ build/precache-manifest.7679041f0cc98578500cec07f9a8df1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20591beed20646a461d9a1c5e13507b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1f5e0da2b240c830285",
+    "url": "/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "94c23aac3c1f25d8f893",
+    "url": "/static/js/2.65d27cc7.chunk.js"
+  },
+  {
+    "revision": "052157e0581e32a2b8fa8f7f1414d257",
+    "url": "/static/js/2.65d27cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f5e0da2b240c830285",
+    "url": "/static/js/main.5f02b1d9.chunk.js"
+  },
+  {
+    "revision": "3a1b67e39924a1407f05",
+    "url": "/static/js/runtime-main.ac52b6b6.js"
+  }
+]);
